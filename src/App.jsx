41a0a4823729011b584e2ff16d670c3dd308fea7
Sonal kumari <115936469+Sonalkri2004@users.noreply.github.com>
2024-11-13 23:21:55 +0530

    basic counter logic using use state hooks.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const App = () => {
+ 
+  const [a, setA] = useState(10)
+
+
   return (
-    <div >
-
-
-      <h1 className='bg-black text-white'>hello how are you?</h1>
-
-
+    <div>
+      <h1>The value of a is {a}</h1>
+      <button onClick={()=>setA(a+10)}>Increment</button>
     </div>
   )
 }
