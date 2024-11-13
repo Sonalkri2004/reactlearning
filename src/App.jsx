10fa29570317a96491e193e3fcc46d6c61966b98
Sonal kumari <115936469+Sonalkri2004@users.noreply.github.com>
2024-11-13 23:06:55 +0530

    project setup completed.

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div >
+
+
+      <h1 className='bg-black text-white'>hello how are you?</h1>
+
+
+    </div>
+  )
+}
+
+export default App
