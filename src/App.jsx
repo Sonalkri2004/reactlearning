26c1df286f1de1974512ee49d6d6050065b2ce5f
Sonal kumari <115936469+Sonalkri2004@users.noreply.github.com>
2024-11-13 23:33:29 +0530

    logic to avoid negative value after decrement.

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,21 @@ import React, { useState } from 'react'
 
 const App = () => {
  
-  const [a, setA] = useState(10)
+  const [a, setA] = useState(10);
+
+  
+//avoid negative value after decrement.
+
+  if(a<0){
+    setA(0);
+  }
 
 
   return (
     <div>
-      <h1>The value of a is {a}</h1>
-      <button onClick={()=>setA(a+10)}>Increment</button>
+      <h1 className='bg-yellow-300'>The value of a is {a}</h1>
+      <button onClick={()=>setA(a+10)} className='bg-green-400 mt-7 ml-7 rounded text-center font-bold p-2'>Increment</button>
+      <button onClick={()=>setA(a-10)} className='bg-green-400 mt-7 ml-7 rounded text-center font-bold p-2'>Decrement</button>
     </div>
   )
 }
