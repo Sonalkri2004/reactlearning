d7c3307ddcff6f56962ef2c3bf9007d570f40789
Sonal kumari <115936469+Sonalkri2004@users.noreply.github.com>
2024-11-15 21:31:15 +0530

    form handling done.

--- src/App.jsx
+++ src/App.jsx
@@ -12,11 +12,29 @@ const App = () => {
   }
 
 
+  const submitHandler = (e)=>{
+    e.preventDefault();
+     console.log("hello")
+  }
+
+
   return (
     <div>
       <h1 className='bg-yellow-300 text-5xl'>The value of a is {a}</h1>
       <button onClick={()=>setA(a+10)} className='bg-green-400 mt-7 ml-7 rounded text-center font-bold p-2'>Increment</button>
       <button onClick={()=>setA(a-10)} className='bg-green-400 mt-7 ml-7 rounded text-center font-bold p-2'>Decrement</button>
+
+
+
+       {/* form handling */}
+
+       <form onSubmit={(e)=>{submitHandler(e)
+       }
+       }>
+        <input className='px-4 rounded py-3 text-xl m-5 bg-black text-white' type='text' placeholder='Enter your name'/>
+        <button className='px-4 py-3 text-xl font-semibold bg-emerald-600 rounded text-white'>Submit</button>
+       </form>
+
     </div>
   )
 }
