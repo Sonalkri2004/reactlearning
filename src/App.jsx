ed0cbf40458eb034b840f417770e64c609509852
Sonal kumari <115936469+Sonalkri2004@users.noreply.github.com>
2024-11-15 21:42:51 +0530

    two way binding .

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import React, { useState } from 'react'
 const App = () => {
  
   const [a, setA] = useState(10);
+  const [username, setUsername] = useState('')
 
 
 //avoid negative value after decrement.
@@ -14,7 +15,8 @@ const App = () => {
 
   const submitHandler = (e)=>{
     e.preventDefault();
-     console.log("hello")
+     console.log(username)
+     setUsername('')
   }
 
 
@@ -31,7 +33,13 @@ const App = () => {
        <form onSubmit={(e)=>{submitHandler(e)
        }
        }>
-        <input className='px-4 rounded py-3 text-xl m-5 bg-black text-white' type='text' placeholder='Enter your name'/>
+        <input 
+        value={username}
+        onChange={(e)=>{
+          setUsername(e.target.value);
+
+        }}
+        className='px-4 rounded py-3 text-xl m-5 bg-black text-white' type='text' placeholder='Enter your name'/>
         <button className='px-4 py-3 text-xl font-semibold bg-emerald-600 rounded text-white'>Submit</button>
        </form>
 
