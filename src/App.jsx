92a18e04276b7311d6a7ba1f564946a783442e19
Sonal kumari <115936469+Sonalkri2004@users.noreply.github.com>
2024-11-16 10:08:18 +0530

    props concept.

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,46 @@
-import React, { useState } from 'react'
+import React from 'react'
+import Card from './components/Card'
 
 const App = () => {
- 
-  const [a, setA] = useState(10);
-  const [username, setUsername] = useState('')
 
-
-//avoid negative value after decrement.
-
-  if(a<0){
-    setA(0);
+ const users=[
+  {
+    "name": "Alice Johnson",
+    "age": 28,
+    "city": "New York",
+    "profession": "Software Engineer"
+  },
+  {
+    "name": "Rahul Sharma",
+    "age": 32,
+    "city": "Mumbai",
+    "profession": "Data Analyst"
+  },
+  {
+    "name": "Emily Davis",
+    "age": 24,
+    "city": "London",
+    "profession": "Graphic Designer"
+  },
+  {
+    "name": "Hiroshi Tanaka",
+    "age": 45,
+    "city": "Tokyo",
+    "profession": "Marketing Manager"
+  },
+  {
+    "name": "Fatima Khalid",
+    "age": 30,
+    "city": "Dubai",
+    "profession": "Architect"
   }
-
-
-  const submitHandler = (e)=>{
-    e.preventDefault();
-     console.log(username)
-     setUsername('')
-  }
-
+]
 
   return (
-    <div>
-      <h1 className='bg-yellow-300 text-5xl'>The value of a is {a}</h1>
-      <button onClick={()=>setA(a+10)} className='bg-green-400 mt-7 ml-7 rounded text-center font-bold p-2'>Increment</button>
-      <button onClick={()=>setA(a-10)} className='bg-green-400 mt-7 ml-7 rounded text-center font-bold p-2'>Decrement</button>
-
-
-
-       {/* form handling */}
-
-       <form onSubmit={(e)=>{submitHandler(e)
-       }
-       }>
-        <input 
-        value={username}
-        onChange={(e)=>{
-          setUsername(e.target.value);
-
-        }}
-        className='px-4 rounded py-3 text-xl m-5 bg-black text-white' type='text' placeholder='Enter your name'/>
-        <button className='px-4 py-3 text-xl font-semibold bg-emerald-600 rounded text-white'>Submit</button>
-       </form>
-
+    <div className='p-10'>
+      {users.map((elem)=>{
+        return <Card user={elem.name} city={elem.city} age={elem.age} profession={elem.profession} />
+      })}
     </div>
   )
 }
