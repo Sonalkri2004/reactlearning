92c3ccbc9fc0b2e1fe8fb5d36f6bf91a4047c85a
Sonal kumari <115936469+Sonalkri2004@users.noreply.github.com>
2024-11-25 10:09:46 +0530

    api call.

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,78 @@
-import React from 'react'
-import Card from './components/Card'
+import axios from 'axios';
+import React, { useState } from 'react'
+// import Card from './components/Card'
 
 const App = () => {
 
- const users=[
-  {
-    "name": "Alice Johnson",
-    "age": 28,
-    "city": "New York",
-    "profession": "Software Engineer"
-  },
-  {
-    "name": "Rahul Sharma",
-    "age": 32,
-    "city": "Mumbai",
-    "profession": "Data Analyst"
-  },
-  {
-    "name": "Emily Davis",
-    "age": 24,
-    "city": "London",
-    "profession": "Graphic Designer"
-  },
-  {
-    "name": "Hiroshi Tanaka",
-    "age": 45,
-    "city": "Tokyo",
-    "profession": "Marketing Manager"
-  },
-  {
-    "name": "Fatima Khalid",
-    "age": 30,
-    "city": "Dubai",
-    "profession": "Architect"
+//  const users=[
+//   {
+//     "name": "Alice Johnson",
+//     "age": 28,
+//     "city": "New York",
+//     "profession": "Software Engineer"
+//   },
+//   {
+//     "name": "Rahul Sharma",
+//     "age": 32,
+//     "city": "Mumbai",
+//     "profession": "Data Analyst"
+//   },
+//   {
+//     "name": "Emily Davis",
+//     "age": 24,
+//     "city": "London",
+//     "profession": "Graphic Designer"
+//   },
+//   {
+//     "name": "Hiroshi Tanaka",
+//     "age": 45,
+//     "city": "Tokyo",
+//     "profession": "Marketing Manager"
+//   },
+//   {
+//     "name": "Fatima Khalid",
+//     "age": 30,
+//     "city": "Dubai",
+//     "profession": "Architect"
+//   }
+// ]
+ 
+
+const [data, setData] = useState([]);
+
+
+  const getData = async ()=>{
+
+    const response= await axios.get('https://picsum.photos/v2/list?page=2&limit=100');
+    setData(response.data);
+    console.log(data);
   }
-]
 
   return (
+
+
+    // <div className='p-10'>
+    //   {users.map((elem,idx)=>{
+    //     return
+    //      <Card key={idx} user={elem.name} city={elem.city} age={elem.age} profession={elem.profession} />
+    //   })}
+    // </div>
+
+     
     <div className='p-10'>
-      {users.map((elem,idx)=>{
-        return <Card key={idx} user={elem.name} city={elem.city} age={elem.age} profession={elem.profession} />
-      })}
+        <button  onClick={getData}  className='bg-cyan-400 text-black font-semibold text-2xl px-6 py-3 rounded active:scale-90'>Get Data</button>
+        <div className='p-5 mt-5 bg-black'>
+              {
+                data.map(function (elem,idx){
+                  return <div key={idx} className='bg-indigo-400 text-black flex items-center px-7 py-6 justify-between w-full rounded mb-3'>
+                               <img className='h-60' src={elem.download_url} />
+                               <h1 className='font-bold text-1xl'>{elem.author}</h1>
+                  </div>
+                })
+              }
+        </div>
     </div>
+
   )
 }
 
