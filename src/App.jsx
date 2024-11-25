e36367c222fe46936030cc14017e24d3733d4fd0
Sonal kumari <115936469+Sonalkri2004@users.noreply.github.com>
2024-11-25 10:16:45 +0530

    use useEffect hook for automatically call the data without clicking the button. Used to handle sideEffect or side work in our project.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 // import Card from './components/Card'
 
 const App = () => {
@@ -45,8 +45,14 @@ const [data, setData] = useState([]);
 
     const response= await axios.get('https://picsum.photos/v2/list?page=2&limit=100');
     setData(response.data);
-    console.log(data);
+    // console.log(data);
   }
+
+
+  useEffect(() => {
+    getData();
+  }, [])
+  
 
   return (
 
